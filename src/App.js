33c7a0eafb677da300141fa8a989d5a6695721bc
Dhayanidhi <dnidhi2710@gmail.com>
2019-05-31 13:25:57 -0400

    Implemented Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import Addtodo from './components/Addtodo';
 import uuid from 'uuid';
-
+import About from './components/pages/About';
 import './App.css';
 
 class App extends Component {
@@ -63,15 +64,23 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <Addtodo addTodo={this.addTodo} />
-          <h2>
-            <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
-          </h2>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <Addtodo addTodo={this.addTodo} />
+                <h4>
+                  <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+                </h4>
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+        <React.Fragment>
+            <h2>About</h2>
+            <p>This is a TodoList app v1.0.0</p>
+        </React.Fragment>
+    )
+}
+export default About;
